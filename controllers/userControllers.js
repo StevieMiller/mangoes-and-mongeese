df031c158503f6958fa80b9f646e0a32270234e1
StevieMiller <stephanie.miller@carlsbadschools.net>
2023-04-06 10:52:12 -0600

    added index.js to models and api, added userControllers

---
+++ controllers/userControllers.js
@@ -0,0 +1,54 @@
+const { User, Thought } = require("../models");
+
+module.exports = {
+  // GET all users
+  getUsers(req, res) {
+    User.find()
+      .then((users) => res.json(users))
+      .catch((err) => res.status(500).json(err));
+  },
+  // GET a single user
+  getSingleUser(req, res) {
+    User.findOne({ _id: req.params.userId })
+      .populate("thoughts")
+      .populate("friends")
+      .select("-__v")
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: "No user with that ID" })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+  // POST a new user
+  createUser(req, res) {
+    User.create(req.body)
+      .then((user) => res.json(user))
+      .catch((err) => res.status(500).json(err));
+  },
+  // PUT to update a user by its _id
+  updateUser(req, res) {
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $set: req.body },
+      { runValidators: true, new: true }
+    )
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: "No user with that ID" })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+  // DELETE a user and associated apps
+  deleteUser(req, res) {
+    User.findOneAndDelete({ _id: req.params.userId })
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: "No user with that ID" })
+          : Application.deleteMany({ _id: { $in: user.applications } })
+      )
+      .then(() => res.json({ message: "User and associated apps deleted!" }))
+      .catch((err) => res.status(500).json(err));
+  },
+};

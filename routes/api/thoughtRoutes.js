e7f410743fdf15bf22f144b6f879ec93372554fc
StevieMiller <stephanie.miller@carlsbadschools.net>
2023-04-07 11:20:59 -0600

    added user and thought routes to api folder

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,28 @@
+const router = require("express").Router();
+const {
+  getThought,
+  getSingleThought,
+  createThought,
+  updateThought,
+  deleteThought,
+  addReaction,
+  removeReaction,
+} = require("../../controllers/thoughtControllers");
+
+// /api/thoughts
+router.route("/").get(getThought).post(createThought);
+
+// /api/thought/:thoughtId
+router
+  .route("/:thoughtId")
+  .get(getSingleThought)
+  .put(updateThought)
+  .delete(deleteThought);
+
+// /api/thoughts/:thoughtId/reactions
+router.route("/:thoughtId/reactions").post(addReaction);
+
+// /api/thoughts/:thoughtId/tags/:reactionId
+router.route("/:thoughtId/reactions/:reactionId").delete(removeReaction);
+
+module.exports = router;

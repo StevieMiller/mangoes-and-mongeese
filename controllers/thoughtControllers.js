765447aee4a4bfddcaab74250185a17ce7f68290
StevieMiller <stephanie.miller@carlsbadschools.net>
2023-04-12 10:14:29 -0600

    final changes

--- controllers/thoughtControllers.js
+++ controllers/thoughtControllers.js
@@ -80,7 +80,7 @@ module.exports = {
   deleteReaction(req, res) {
     Thought.findOneAndUpdate(
       { _id: req.params.thoughtId },
-      { $pull: { reactions: { reactionId: req.params.reactionId } } },
+      { $pull: { reactions: { reactionId: req.body.reactionId } } },
       { runValidators: true, new: true }
     )
       .then((thought) =>
